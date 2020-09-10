42c498b02b2cf50c9155ce4b9adb21b2813f5ba6
crazytechie1990 <60341560+crazytechie1990@users.noreply.github.com>
2020-09-10 16:35:33 +0100

    Update logdump.py

--- logdump.py
+++ logdump.py
@@ -2,8 +2,11 @@
 import argparse
 import psycopg2 as pg
 
-parser = argparse.ArgumentParser(description=
-                                 """
+####################
+# Argument parsing #
+####################
+
+parser = argparse.ArgumentParser(description="""
 Reads log table. By default displays the last 1 hour log for all categories with all severities.
 """)
 parser.add_argument('-s', '--start_time', required=False, help="provide time in format of "
@@ -18,13 +21,21 @@ parser.add_argument('-c', '--category', required=True,
                     CreateConfig,CreateMedia,ExportImages,ImportImages,HouseKeeping
                     """)
 args = parser.parse_args()
-sql_insert_query = """ SELECT job_id,insert_time,category,message from log
+
+# SQL query to filter out logs from log table.
+SQLQuery = """ SELECT job_id,insert_time,category,message from log
                        where category=%s and severity=%s and insert_time between %s AND %s;
                        """
-insert_tuple_1 = (args.category, args.severity, args.start_time, args.end_time)
+# Tuple of incoming arguments to be inserted to the sql query.
+QueryTuple = (args.category, args.severity, args.start_time, args.end_time)
 
 
 def db_connect():
+    """
+    Function to connect to the postgres database, create a cursor and read all the rows based on the filter
+    provided.
+    :return:
+    """
     # connect to the TZM Database
     try:
         conn = pg.connect(
@@ -34,7 +45,8 @@ def db_connect():
             password="")
         # create a cursor
         cur = conn.cursor()
-        cur.execute(sql_insert_query, insert_tuple_1)
+        cur.execute(SQLQuery, QueryTuple)
+        # Read all the rows
         rows = cur.fetchall()
         print("=" * 135)
         print('{:<10s}{:>20s}{:>29s}{:^50s}'.format("JobID", "Time", "Category", "Message"))
